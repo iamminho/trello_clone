80ba902649c87d1b5d5d7cb64407bd885e628f43
iamminho <minho100227@kakao.com>
2023-06-06 12:04:01 +0900

    Typing the Props

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,12 @@
-import React from "react";
 import styled from "styled-components";
+import Circle from "./Circle";
 
-const Wrapper = styled.div`
-  display: flex;
-  height: 100vh;
-  width: 100vw;
-  justify-content: center;
-  align-items: center;
-  background-color: ${(props) => props.theme.backgroundColor};
-`;
-
-const Title = styled.h1`
-  color: ${(props) => props.theme.textColor};
-`;
 function App() {
   return (
-    <Wrapper>
-      <Title>TypeScript</Title>
-    </Wrapper>
+    <div>
+      <Circle bgColor="teal" />
+      <Circle bgColor="tomato" />
+    </div>
   );
 }
 
