eb5c62f7c1321166aa49d5f3614feb93cc692ed2
iamminho <minho100227@kakao.com>
2023-06-06 17:16:25 +0900

    Themes

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,18 @@
 import { useState } from "react";
 import styled from "styled-components";
 
-function App() {
-  const [value, setValue] = useState("");
-  const onChange = (event: React.FormEvent<HTMLInputElement>) => {
-    const { value } = event.currentTarget;
-    setValue(value);
-  };
-  const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
-    event.preventDefault();
-    console.log("hello", value);
-  };
+const Container = styled.div`
+  background-color: ${(props) => props.theme.bgColor};
+`;
+const H1 = styled.h1`
+  color: ${(props) => props.theme.textColor};
+`;
 
+function App() {
   return (
-    <div>
-      <form onSubmit={onSubmit}>
-        <input
-          value={value}
-          onChange={onChange}
-          type="text"
-          placeholder="username"
-        />
-        <button>Log in</button>
-      </form>
-    </div>
+    <Container>
+      <H1>Protocol </H1>
+    </Container>
   );
 }
 
