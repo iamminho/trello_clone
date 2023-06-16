4e29fe0ddcffa1058f35d07e479267ba2d8c544f
iamminho <minho100227@kakao.com>
2023-06-16 18:54:24 +0900

    Categories

--- src/components/ToDo.tsx
+++ src/components/ToDo.tsx
@@ -1,13 +1,21 @@
-import { useRecoilValue } from "recoil";
-import { IToDo } from "./atoms";
+import { useRecoilValue, useSetRecoilState } from "recoil";
+import { IToDo, toDoState } from "./atoms";
 
-function ToDo({ text }: IToDo) {
+function ToDo({ text, category, id }: IToDo) {
+  const setToDos = useSetRecoilState(toDoState);
+  const onClick = (newCategory: IToDo["category"]) => {};
   return (
     <li>
       <span>{text}</span>
-      <button>todo</button>
-      <button>Done</button>
-      <button>Doing</button>
+      {category !== "TODO" && (
+        <button onClick={() => onClick("TODO")}>Todo</button>
+      )}
+      {category !== "DOING" && (
+        <button onClick={() => onClick("DOING")}>Doing</button>
+      )}
+      {category !== "DONE" && (
+        <button onClick={() => onClick("DONE")}>Done</button>
+      )}
     </li>
   );
 }
